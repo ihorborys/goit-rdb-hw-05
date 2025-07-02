50eaf76a7ce1d91c8a02dc6c486275509ac8bc55
ihorborys <ihorborys@gmail.com>
2025-07-02 13:27:14 +0300

    Completed task 4

---
+++ ASSETS/scripts/script_4.sql
@@ -0,0 +1,12 @@
+WITH TemporalTable AS (
+SELECT 
+        *
+    FROM
+        order_details
+    WHERE
+        quantity > 10)
+SELECT 
+    order_id, AVG(quantity) AS avg_quantity
+FROM
+    TemporalTable
+GROUP BY order_id
